cf7443a8c644f808fcd7f1793fc500cc1a4e2791
kevindet <kevindet@hotmail.com>
2016-10-07 14:40:43 -0700

    Java Spark Code with working Get method and Database connectivity

--- TodoApp.java
+++ TodoApp.java
@@ -1,9 +1,62 @@
 package com.dat605;
 
+import java.sql.*; // for standard JDBC programs
+import java.util.ArrayList;
+import java.util.Collection;
+import org.json.simple.JSONObject;
 import static spark.Spark.*;
 
 public class TodoApp {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+
+	private static Connection connection;
+	private static String url = "jdbc:mysql://localhost:3306/TodoApp";
+	private static String username = "root";
+	private static String password = "dat605";
+
+	public static void main(String[] args) {
+
+		// Connect to Database
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Connection Passed");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed");
+			e.printStackTrace();
+		}
+
+		post("/post", (ICRoute, response) -> {
+			response.body("Test");
+			return response;
+			// Create something
+			});
+
+		get("/test",
+				(request, response) -> {
+					JSONObject obj = new JSONObject();
+					try {
+
+						PreparedStatement st = connection
+								.prepareStatement("SELECT * FROM Todo");
+						ResultSet r1 = st.executeQuery();
+						ResultSetMetaData metaData = r1.getMetaData();
+						int columnCount = metaData.getColumnCount();
+
+						while (r1.next()) {
+							int i = 1;
+							while (i <= columnCount) {
+								String value = r1.getString(i++);
+								String key = r1.getString(i++);
+								obj.put(value, key);
+							}
+						}
+					} catch (IllegalStateException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					return obj;
+				});
+	}
 }
