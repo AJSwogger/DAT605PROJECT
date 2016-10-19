f51e9e2ee46fa7be9497c4b7c0ff2d13916907f4
kevindet <kevindet@hotmail.com>
2016-10-18 21:01:05 -0700

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,18 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+	
+	//Database connection credentials
 	private Connection connection;
 	private final String url = "jdbc:mysql://localhost:3306/TodoApp";
 	private final String username = "root";
 	private final String password = "dat605";
 
+	//Connect to Database
 	public Connection connect() {
 
 		try {
 			connection = DriverManager.getConnection(url, username, password);
-			System.out.println("Connection Passed");
 		} catch (SQLException e) {
-			System.out.println("Connection Failed");
 			e.printStackTrace();
 		} 
 		return connection;
