b2fed6a761747a3663f74be06c9b839ff6d39b46
kevindet <kevindet@hotmail.com>
2016-10-18 21:02:11 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package com.dat605;
 
 public class Main {
 
+	// Instantiate DatabaseConnection, TodoController and TodoItemService
 	public static void main(String[] args) {
 		DatabaseConnection db = new DatabaseConnection();
 		new TodoController(new TodoItemService(db), db);
